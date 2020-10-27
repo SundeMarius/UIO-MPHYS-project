Fixed one error in the KL-div calculation (forgot to multiply with the bin width in the sum over bins)

lib/statistics_tools.py
@@ -25,7 +25,7 @@ def combine_two_bins(count, bin_edges, i):
     width_right = new_bins[(i+1)%N] - new_bins[i]
 
     width = new_bins[i] - new_bins[i-1]
-    # Combine bin i with the smallest of the neighbor bins.
+    # Combine bin i with the smallest of the neighbor bins (then scale it with the average count value between those two).
     if bin_right >= bin_left or bin_right == 0:
         new_bins.pop(i-1)
         new_count[i-1] *= width_left/(width + width_left)
@@ -43,8 +43,8 @@ def combine_two_bins(count, bin_edges, i):
 # Kullback-Leibler divergence
 def KL_div(P, Q, base_two=False):
     """
-    :param P: normalised numpy.histogram object (bins sum to 1)
-    :param Q: normalised numpy.histogram object (bins sum to 1)
+    :param P: numpy.histogram object (bins sum to 1)
+    :param Q: numpy.histogram object (bins sum to 1)
     :param base_two: Set True if you want rel.entr in #bits (default is #nats)
     
     :return : The Kullback-Leibler divergence (avg. relative entropy) between the two input distributions 
@@ -55,7 +55,7 @@ def KL_div(P, Q, base_two=False):
 
     # Check if Q-histogram contains empty bins. Resize bins in P and Q if so.
     while 0 in Q_count:
-        i = list(Q_count).index(0.)
+        i = list(Q_count).index(0)
         Q_count, Q_bins = combine_two_bins(Q_count, Q_bins, i)
         
         #Do the same change in P-histogram
@@ -63,7 +63,7 @@ def KL_div(P, Q, base_two=False):
     
     # Check P-histogram as well     
     while 0 in P_count:
-        i = list(P_count).index(0.)
+        i = list(P_count).index(0)
         P_count, P_bins = combine_two_bins(P_count, P_bins, i)
         
         #Do the same change in Q-histogram
@@ -87,7 +87,7 @@ def KL_div(P, Q, base_two=False):
 
     #Calculate KL-divergence and return the value
     rel_entropy_array = rel_entr(P_count, Q_count)
-    kl_d = np.sum(rel_entropy_array) 
+    kl_d = np.sum(rel_entropy_array * np.diff(P_bins)) 
     
     if base_two:
         return kl_d/np.log(2.)

plot_invariant_mass.py
@@ -83,21 +83,21 @@ for f, filename in enumerate(filenames):
         np.savetxt(res_file, inv_mass, fmt='%e', header="Invariant mass of the %d final state particles of this process"%n_particles)
         print("Stored calculations in %s"%res_file)
 
-    counts, bins = np.histogram(inv_mass, bins='auto')
+    counts, bins = np.histogram(inv_mass, bins=120)
     histograms.append([counts, bins])
     
     #Add histogram to plot
     plot.add_histogram(counts, bins, label=labels[f], alpha=.5)
 
 # Calculate KL-divergence between LO and NLO distributions
-LO_hist = histograms[0]  # tuple (count, bin_edges)
-NLO_hist = histograms[1]  # tuple (count, bin_edges)
+LO_hist = histograms[0]  # tuple (count, bin_edges), Q-distribution representing the model
+NLO_hist = histograms[1]  # tuple (count, bin_edges), P-distribution representing the data
 
 # Normalise them before doing KL-div (unecessary since KL_div function does this already)
 LO_hist[0] = LO_hist[0]/np.sum(LO_hist[0] * np.diff(LO_hist[1]))
 NLO_hist[0] = NLO_hist[0]/np.sum(NLO_hist[0] * np.diff(NLO_hist[1]))
 
-kl_div = st.KL_div(LO_hist, NLO_hist)
+kl_div = st.KL_div(NLO_hist, LO_hist)
 print("KL-divergence between LO and NLO distributions: %1.4f"%kl_div)
 
 # Plot data

tests.py
@@ -24,10 +24,10 @@ def test_4momentum():
 def test_KL_div():
     mu1 = 0
     mu2 = 0
-    sig1 = 1
-    sig2 = 2
-    N_sampl = 100_000
-    n_bins = 130
+    sig1 = 2
+    sig2 = 5
+    N_sampl = 10_000_000
+    n_bins = 3100
     dkl_analytical = ((sig1/sig2)**2 + (mu1 - mu2)**2 / sig2**2 - 1 + np.log(sig2**2/sig1**2))/2.
 
     print("# samples: %d"%N_sampl)
@@ -35,10 +35,10 @@ def test_KL_div():
     print("D_KL analytic: %f\n"%dkl_analytical)
 
     avg_dkl = 0
-    N = 5
+    N = 10
     for i in range(N):
-        p = st.sample_normal(mu1, sig1, N_sampl, n_bins, -15, 15, density=True)
-        q = st.sample_normal(mu2, sig2, N_sampl, n_bins, -15, 15, density=True)
+        p = st.sample_normal(mu1, sig1, N_sampl, n_bins)
+        q = st.sample_normal(mu2, sig2, N_sampl, n_bins)
         dkl_numeric = st.KL_div(p, q)
         avg_dkl += dkl_numeric
         print(dkl_numeric)
@@ -51,7 +51,7 @@ def test_plot():
     x = np.linspace(0, 2*np.pi)
     y = np.exp(-x)
 
-    counts, bins = st.sample_normal(0, 1, 1_000_000, 180, density=True)
+    counts, bins = st.sample_normal(0, 1, 100_000, 60, density=True)
 
     while 0 in counts:
         i = list(counts).index(0.)
@@ -74,9 +74,9 @@ def test_plot():
 
 spacer = 20*'-'
 
-test_4momentum()
+#test_4momentum()
 print(spacer)
 test_KL_div()
 print(spacer)
-test_plot()
+#test_plot()
 print("Tests done.")